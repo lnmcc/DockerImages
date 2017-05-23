9d30dd3e7310c99952c3eaaf5202f698f3ad7b06
lnmcc <lnmcc@hotmail.com>
2017-05-23 10:02:39 +0800

    fix apache

--- gerritwithapache/gerrit-entrypoint.sh
+++ gerritwithapache/gerrit-entrypoint.sh
@@ -1,1 +1,141 @@
-#/bin/env bash
+#!/usr/bin/env sh
+set -e
+
+set_gerrit_config() {
+   git config -f "${GERRIT_SITE}/etc/gerrit.config" "$@"
+}
+
+set_secure_config() {
+   git config -f "${GERRIT_SITE}/etc/secure.config" "$@"
+}
+
+if [ -n "${JAVA_HEAPLIMIT}" ]; then
+  JAVA_MEM_OPTIONS="-Xmx${JAVA_HEAPLIMIT}"
+fi
+
+if [ "$1" = "/gerrit-start.sh" ]; then
+  # If you're mounting ${GERRIT_SITE} to your host, you this will default to root.
+  # This obviously ensures the permissions are set correctly for when gerrit starts.
+  #chown -R ${GERRIT_USER} "${GERRIT_SITE}"
+
+  # Initialize Gerrit if ${GERRIT_SITE}/git is empty.
+  if [ -z "$(ls -A "$GERRIT_SITE/git")" ]; then
+    echo "First time initialize gerrit..."
+    java ${JAVA_OPTIONS} ${JAVA_MEM_OPTIONS} -jar "${GERRIT_WAR}" init --batch --no-auto-start -d "${GERRIT_SITE}" ${GERRIT_INIT_ARGS}
+    #All git repositories must be removed when database is set as postgres or mysql
+    #in order to be recreated at the secondary init below.
+    #Or an execption will be thrown on secondary init.
+    [ ${#DATABASE_TYPE} -gt 0 ] && rm -rf "${GERRIT_SITE}/git"
+  fi
+
+  # Install plugs
+  tar xvf /plugins.tar -C $GERRIT_SITE/plugins/ 
+  rm -f /plugins.tar
+
+  # Provide a way to customise this image
+  echo
+  for f in /docker-entrypoint-init.d/*; do
+    case "$f" in
+      *.sh)    echo "$0: running $f"; .  "$f" ;;
+      *.nohup) echo "$0: running $f"; nohup  "$f" & ;;
+      *)       echo "$0: ignoring $f" ;;
+    esac
+    echo
+  done
+
+  #Customize gerrit.config
+
+  #Section gerrit
+  [ -z "${WEBURL}" ] || set_gerrit_config gerrit.canonicalWebUrl "${WEBURL}"
+  [ -z "${GITHTTPURL}" ] || set_gerrit_config gerrit.gitHttpUrl "${GITHTTPURL}"
+
+  #Section sshd
+  [ -z "${LISTEN_ADDR}" ] || set_gerrit_config sshd.listenAddress "${LISTEN_ADDR}"
+
+  #Section database
+  if [ "${DATABASE_TYPE}" = 'postgresql' ]; then
+    set_gerrit_config database.type "${DATABASE_TYPE}"
+    [ -z "${DB_PORT_5432_TCP_ADDR}" ]    || set_gerrit_config database.hostname "${DB_PORT_5432_TCP_ADDR}"
+    [ -z "${DB_PORT_5432_TCP_PORT}" ]    || set_gerrit_config database.port "${DB_PORT_5432_TCP_PORT}"
+    [ -z "${DB_ENV_POSTGRES_DB}" ]       || set_gerrit_config database.database "${DB_ENV_POSTGRES_DB}"
+    [ -z "${DB_ENV_POSTGRES_USER}" ]     || set_gerrit_config database.username "${DB_ENV_POSTGRES_USER}"
+    [ -z "${DB_ENV_POSTGRES_PASSWORD}" ] || set_secure_config database.password "${DB_ENV_POSTGRES_PASSWORD}"
+  fi
+
+  #Section database
+  if [ "${DATABASE_TYPE}" = 'mysql' ]; then
+    set_gerrit_config database.type "${DATABASE_TYPE}"
+    [ -z "${DB_PORT_3306_TCP_ADDR}" ] || set_gerrit_config database.hostname "${DB_PORT_3306_TCP_ADDR}"
+    [ -z "${DB_PORT_3306_TCP_PORT}" ] || set_gerrit_config database.port "${DB_PORT_3306_TCP_PORT}"
+    [ -z "${DB_ENV_MYSQL_DB}" ]       || set_gerrit_config database.database "${DB_ENV_MYSQL_DB}"
+    [ -z "${DB_ENV_MYSQL_USER}" ]     || set_gerrit_config database.username "${DB_ENV_MYSQL_USER}"
+    [ -z "${DB_ENV_MYSQL_PASSWORD}" ] || set_secure_config database.password "${DB_ENV_MYSQL_PASSWORD}"
+  fi
+
+  #Section auth
+  [ -z "${AUTH_TYPE}" ]           || set_gerrit_config auth.type "${AUTH_TYPE}"
+  [ -z "${AUTH_HTTP_HEADER}" ]    || set_gerrit_config auth.httpHeader "${AUTH_HTTP_HEADER}"
+  [ -z "${AUTH_EMAIL_FORMAT}" ]   || set_gerrit_config auth.emailFormat "${AUTH_EMAIL_FORMAT}"
+  [ -z "${AUTH_GIT_BASIC_AUTH}" ] || set_gerrit_config auth.gitBasicAuth "${AUTH_GIT_BASIC_AUTH}"
+
+  #Section OAUTH general
+  if [ "${AUTH_TYPE}" = 'OAUTH' ]  ; then
+    ${GERRIT_USER} cp -f ${GERRIT_HOME}/gerrit-oauth-provider.jar ${GERRIT_SITE}/plugins/gerrit-oauth-provider.jar
+    [ -z "${OAUTH_ALLOW_EDIT_FULL_NAME}" ]     || set_gerrit_config oauth.allowEditFullName "${OAUTH_ALLOW_EDIT_FULL_NAME}"
+    [ -z "${OAUTH_ALLOW_REGISTER_NEW_EMAIL}" ] || set_gerrit_config oauth.allowRegisterNewEmail "${OAUTH_ALLOW_REGISTER_NEW_EMAIL}"
+
+    # Google
+    [ -z "${OAUTH_GOOGLE_RESTRICT_DOMAIN}" ]   || set_gerrit_config plugin.gerrit-oauth-provider-google-oauth.domain "${OAUTH_GOOGLE_RESTRICT_DOMAIN}"
+    [ -z "${OAUTH_GOOGLE_CLIENT_ID}" ]         || set_gerrit_config plugin.gerrit-oauth-provider-google-oauth.client-id "${OAUTH_GOOGLE_CLIENT_ID}"
+    [ -z "${OAUTH_GOOGLE_CLIENT_SECRET}" ]     || set_gerrit_config plugin.gerrit-oauth-provider-google-oauth.client-secret "${OAUTH_GOOGLE_CLIENT_SECRET}"
+    [ -z "${OAUTH_GOOGLE_LINK_OPENID}" ]       || set_gerrit_config plugin.gerrit-oauth-provider-google-oauth.link-to-existing-openid-accounts "${OAUTH_GOOGLE_LINK_OPENID}"
+
+    # Github
+    [ -z "${OAUTH_GITHUB_CLIENT_ID}" ]         || set_gerrit_config plugin.gerrit-oauth-provider-github-oauth.client-id "${OAUTH_GITHUB_CLIENT_ID}"
+    [ -z "${OAUTH_GITHUB_CLIENT_SECRET}" ]     || set_gerrit_config plugin.gerrit-oauth-provider-github-oauth.client-secret "${OAUTH_GITHUB_CLIENT_SECRET}"
+
+    # GitLab
+    [ -z "${OAUTH_GITLAB_ROOT_URL}" ]         || set_gerrit_config plugin.gerrit-oauth-provider-gitlab-oauth.root-url "${OAUTH_GITLAB_ROOT_URL}"
+    [ -z "${OAUTH_GITLAB_CLIENT_ID}" ]         || set_gerrit_config plugin.gerrit-oauth-provider-gitlab-oauth.client-id "${OAUTH_GITLAB_CLIENT_ID}"
+    [ -z "${OAUTH_GITLAB_CLIENT_SECRET}" ]     || set_gerrit_config plugin.gerrit-oauth-provider-gitlab-oauth.client-secret "${OAUTH_GITLAB_CLIENT_SECRET}"
+  fi
+
+  #Section container
+  [ -z "${JAVA_HEAPLIMIT}" ] || set_gerrit_config container.heapLimit "${JAVA_HEAPLIMIT}"
+  [ -z "${JAVA_OPTIONS}" ]   || set_gerrit_config container.javaOptions "${JAVA_OPTIONS}"
+  [ -z "${JAVA_SLAVE}" ]     || set_gerrit_config container.slave "${JAVA_SLAVE}"
+
+  #Section sendemail
+  if [ -z "${SMTP_SERVER}" ]; then
+    set_gerrit_config sendemail.enable false
+  else
+    set_gerrit_config sendemail.enable true
+    set_gerrit_config sendemail.smtpServer "${SMTP_SERVER}"
+    if [ "smtp.gmail.com" = "${SMTP_SERVER}" ]; then
+      echo "gmail detected, using default port and encryption"
+      set_gerrit_config sendemail.smtpServerPort 587
+      set_gerrit_config sendemail.smtpEncryption tls
+    fi
+    [ -z "${SMTP_SERVER_PORT}" ] || set_gerrit_config sendemail.smtpServerPort "${SMTP_SERVER_PORT}"
+    [ -z "${SMTP_USER}" ]        || set_gerrit_config sendemail.smtpUser "${SMTP_USER}"
+    [ -z "${SMTP_PASS}" ]        || set_secure_config sendemail.smtpPass "${SMTP_PASS}"
+    [ -z "${SMTP_ENCRYPTION}" ]      || set_gerrit_config sendemail.smtpEncryption "${SMTP_ENCRYPTION}"
+    [ -z "${SMTP_CONNECT_TIMEOUT}" ] || set_gerrit_config sendemail.connectTimeout "${SMTP_CONNECT_TIMEOUT}"
+    [ -z "${SMTP_FROM}" ]            || set_gerrit_config sendemail.from "${SMTP_FROM}"
+  fi
+
+  #Section user
+    [ -z "${USER_NAME}" ]             || set_gerrit_config user.name "${USER_NAME}"
+    [ -z "${USER_EMAIL}" ]            || set_gerrit_config user.email "${USER_EMAIL}"
+    [ -z "${USER_ANONYMOUS_COWARD}" ] || set_gerrit_config user.anonymousCoward "${USER_ANONYMOUS_COWARD}"
+
+  #Section plugins
+  set_gerrit_config plugins.allowRemoteAdmin true
+
+  #Section plugin events-log
+  set_gerrit_config plugin.events-log.storeUrl "jdbc:h2:${GERRIT_SITE}/db/ChangeEvents"
+
+  #Section httpd
+  [ -z "${HTTPD_LISTENURL}" ] || set_gerrit_config httpd.listenUrl "${HTTPD_LISTENURL}"
+fi
+exec "$@"
---
+++ gerritwithapache/start-apache.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+a2enmod proxy proxy_http
+service  apache2 start
